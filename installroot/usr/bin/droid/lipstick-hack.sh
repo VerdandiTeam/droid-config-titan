6c9cdeb7718937cd041267234ada94c42937ac11
Mister_Magister <michal_sz13@go2.pl>
2016-07-01 17:49:38 +0200

    added gitignore

--- installroot/usr/bin/droid/lipstick-hack.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# surfaceflinger needs to run for a bit, in order for sailfish graphics to work
-# very weird, proper solution needed
-/system/bin/surfaceflinger &
-sleep 5
-killall surfaceflinger
